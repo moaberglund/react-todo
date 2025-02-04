bc11382ae743e7c5c74949904f9cb518ef4d50d4
moaberglund <moa.21@hotmail.com>
2025-02-04 15:42:26 +0100

    interface and state

--- src/components/ToDoForm.tsx
+++ src/components/ToDoForm.tsx
@@ -1,9 +1,25 @@
 import './ToDoForm.scss'
+import { useState } from 'react'
+import * as Yup from 'yup'
 
 const ToDoForm = () => {
+
+    interface FormData {
+        title: string,
+        description: string,
+        completed: boolean
+    }
+
+    // State
+    const [formData, setFormData] = useState<FormData>({
+        title: '',
+        description: '',
+        completed: false
+    })
+    
   return (
     <form className="todo-form">
-        
+
         <label htmlFor="title">Title</label>
         <input type="text" id="title" name="title" />
 
