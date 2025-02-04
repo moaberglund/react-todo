5c66e6fd2dfdcb66f1ce238e1caee4aa50ca0cf4
moaberglund <moa.21@hotmail.com>
2025-02-04 15:52:07 +0100

    error messages span tagg

--- src/components/ToDoForm.tsx
+++ src/components/ToDoForm.tsx
@@ -36,9 +36,11 @@ const ToDoForm = () => {
 
         <label htmlFor="title">Title</label>
         <input type="text" id="title" name="title" value={formData.title} onChange={(event) => setFormData({ ...formData, title: event.target.value })} />
+        {errors.name && <span className="error-message">{errors.name}</span>}
 
         <label htmlFor="description">Description</label>
         <textarea id="description" name="description" value={formData.description} onChange={(event) => setFormData({ ...formData, description: event.target.value })}  />
+        {errors.description && <span className="error-message">{errors.description}</span>}
 
         <button type="submit">Add</button>
 
